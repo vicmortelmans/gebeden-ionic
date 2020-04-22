ab0ead502acc622588c017e4a9b70a7aa3b53a58
Vic Mortelmans <vicmortelmans@gmail.com>
2020-04-23 00:51:40 +0200

    1

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,26 +1,49 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
 import React from 'react';
 import './Home.css';
+import structure from '../data/structure.json'
+import { CarouselProvider, Slider, Slide, ButtonBack, ButtonNext, DotGroup } from 'pure-react-carousel';
+import 'pure-react-carousel/dist/react-carousel.es.css';
 
 const Home: React.FC = () => {
   return (
-    <IonPage>
-      <IonHeader>
-        <IonToolbar>
-          <IonTitle>Blank</IonTitle>
-        </IonToolbar>
-      </IonHeader>
-      <IonContent>
-        <IonHeader collapse="condense">
-          <IonToolbar>
-            <IonTitle size="large">Blank</IonTitle>
-          </IonToolbar>
-        </IonHeader>
-        Hello World!!
-        I'm here!
-        Greetings!
-      </IonContent>
-    </IonPage>
+    <>
+    
+      <CarouselProvider
+        naturalSlideWidth={1}
+        naturalSlideHeight={1}
+        totalSlides={3}
+        isIntrinsicHeight={true}
+      >
+        <p>test</p>
+        <Slider>
+          <Slide index={0} style={{backgroundColor: "blue"}}>I am the first Slide.</Slide>
+          <Slide index={1} style={{backgroundColor: "red"}}>I am the second Slide.</Slide>
+          <Slide index={2} style={{backgroundColor: "green"}}>I am the third Slide.</Slide>
+        </Slider>
+        <ButtonBack>Back</ButtonBack>
+        <ButtonNext>Next</ButtonNext>
+        <DotGroup/>
+        <p>test</p>
+      </CarouselProvider>
+      <p>second:</p>
+      <CarouselProvider
+        naturalSlideWidth={1}
+        naturalSlideHeight={1}
+        totalSlides={3}
+        isIntrinsicHeight={true}
+      >
+        <p>test</p>
+        <Slider>
+          <Slide index={0} style={{backgroundColor: "blue"}}>I am the first Slide.</Slide>
+          <Slide index={1} style={{backgroundColor: "red"}}>I am the second Slide.</Slide>
+          <Slide index={2} style={{backgroundColor: "green"}}>I am the third Slide.</Slide>
+        </Slider>
+        <ButtonBack>Back</ButtonBack>
+        <ButtonNext>Next</ButtonNext>
+        <DotGroup/>
+        <p>test</p>
+      </CarouselProvider>
+    </>
   );
 };
 
