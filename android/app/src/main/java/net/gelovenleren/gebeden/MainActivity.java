831153ece313724899ee628a19a2457fedc93053
Vic Mortelmans <vicmortelmans@gmail.com>
2026-09-15 20:08:38 +0200

    modernizing android app

---
+++ android/app/src/main/java/net/gelovenleren/gebeden/MainActivity.java
@@ -0,0 +1,12 @@
+package net.gelovenleren.gebeden;
+
+import android.os.Bundle;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {
+  @Override
+  public void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+  }
+}
