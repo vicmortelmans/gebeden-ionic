d0f7aed31f40bda9de397c966e9277c870f19a8e
Vic Mortelmans <vicmortelmans@gmail.com>
2026-09-17 21:18:39 +0200

    always light mode

--- android/app/src/main/java/net/gelovenleren/gebeden/MainActivity.java
+++ android/app/src/main/java/net/gelovenleren/gebeden/MainActivity.java
@@ -2,11 +2,14 @@ package net.gelovenleren.gebeden;
 
 import android.os.Bundle;
 
+import androidx.appcompat.app.AppCompatDelegate;
+
 import com.getcapacitor.BridgeActivity;
 
 public class MainActivity extends BridgeActivity {
   @Override
   public void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
+    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
   }
 }
